aab68dd92bc38694a1aa48f983cca1e8a56fe754
Min-Uk.Lee <makerdark98@gmail.com>
2020-09-19 19:26:15 +0900

    src: change local history structure (unstable)

--- src/static/js/localhistory.js
+++ src/static/js/localhistory.js
@@ -1,10 +1,28 @@
-function isValidLocalStorage() {
-  return localStorage.md98wiki === "true";
+function getLocalStorageVersion() {
+  if (localStorage.md98wiki === "true") return "v1";
+  else if (localStorage.md98wiki === "v2") return "v2";
+  return false;
+}
+function localStorageUpgrade(version) {
+  try {
+    if (version !== "v2") throw "invalid version";
+    if (localStorage.md98wiki === "v1") {
+      localStorage.md98wiki = "v2";
+      localStorage.md98wikiData = JSON.stringify(
+        JSON.parse(localStorage.md98wikiData).map(e => ({
+          name : e,
+          link : `/wiki/${e}`,
+        })));
+    }
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
 }
 
 function initLocalStorage() {
   try {
-    localStorage.md98wiki = "true";
+    localStorage.md98wiki = "v2";
     localStorage.md98wikiData = JSON.stringify([]);
     return true;
   } catch (err) {
@@ -15,27 +33,32 @@ function initLocalStorage() {
 
 function getHistory() {
   try {
-    if (isValidLocalStorage()) {
-      return JSON.parse(localStorage.md98wikiData);
-    } else {
-      return [];
+    var localStorageVersion = getLocalStorageVersion();
+    if (localStorageVersion === false) {
+      initLocalStorage();
+    } else if (localStorageVersion !== "v2") {
+      localStorageUpgrade("v2");
     }
+    return JSON.parse(localStorage.md98wikiData);
   } catch (err) {
     console.error(err);
     return [];
   }
 }
 
-function pushHistory(wikilink) {
+function pushHistory(name, link) {
   try {
-    if (!isValidLocalStorage()) {
+    var localStorageVersion = getLocalStorageVersion();
+    if (localStorageVersion === false) {
       initLocalStorage();
+    } else if (localStorageVersion !== "v2") {
+      localStorageUpgrade("v2");
     }
 
     var originalHistory = getHistory();
-    var beforeIdx = originalHistory.indexOf(wikilink);
+    var beforeIdx = originalHistory.indexOf({name, link});
     if (beforeIdx > -1) originalHistory.splice(beforeIdx, 1);
-    originalHistory.push(wikilink);
+    originalHistory.push({name, link});
     localStorage.md98wikiData = JSON.stringify(originalHistory);
 
     return true;
@@ -48,9 +71,7 @@ function pushHistory(wikilink) {
 function showAllHistory(container, show) {
   var hist = getHistory();
   if (show) {
-    container.innerHTML = hist.slice(0, hist.length-3).map(h=> `<li class="breadcrumb-item">[[${h}]]{${h.slice(0, 10)}}</li>`).join('\n');
-
-    convertWikiLink(container);
+    container.innerHTML = hist.slice(0, hist.length-3).map(h=> `<li class="breadcrumb-item"><a href="${h.link}">${h.name.slice(0, 10)}</a></li>`).join('\n');
   } else {
     container.innerHTML = '...';
   }
@@ -61,11 +82,11 @@ function displayHistory(container) {
     var maxLength = 3;
     if (container) {
       var hist = getHistory();
-      
+
       container.innerHTML = '<nav aria-label="breadcrumb"><ol class="breadcrumb">'
         + (hist.length > 3 ? '<li style="cursor:pointer;" class="hidden-history">...</li>' : "")
-        + hist.slice(-3).map(h =>`<li class="breadcrumb-item">[[${h}]]{${h.slice(0, 10)}}</li>`).join('\n');
-        + '</ol></nav>';
+        + hist.slice(-3).map(h =>`<li class="breadcrumb-item"><a href="${h.link}">${h.name.slice(0, 10)}</a></li>`).join('\n');
+      + '</ol></nav>';
 
     }
     return true;
